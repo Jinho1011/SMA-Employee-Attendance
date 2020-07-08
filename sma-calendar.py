524e389f3a10e321e7f837b6f0db8a81f8588b6d
jinho1011 <gino9940@gmail.com>
2020-07-08 16:48:21 +0900

    Implement Get Today's Staff in sma-calendar.py

--- sma-calendar.py
+++ sma-calendar.py
@@ -1,54 +1,56 @@
 from __future__ import print_function
-import datetime
-import pickle
-import os.path
+from pytz import timezone, utc
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+import datetime
+import pickle
+import os.path
 
 
-def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
+def get_today_events_from_google_calendar():
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists('.config/token-calendar.pickle'):
+        with open('.config/token-calendar.pickle', 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                '.config/credentials-calendar.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open('.config/token-calendar.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time`
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='pbinkbigkjdvo9tvb72filfcpo@group.calendar.google.com', timeMin=now,
-                                          maxResults=10, singleEvents=True,
+    today = datetime.date.today()
+    from_ = datetime.datetime(today.year, today.month, today.day, 0, 0, 0,
+                              tzinfo=datetime.timezone.utc).isoformat()
+    to_ = datetime.datetime(today.year, today.month, today.day, 11, 59, 59,
+                            tzinfo=datetime.timezone.utc).isoformat()
+
+    events_result = service.events().list(calendarId='pbinkbigkjdvo9tvb72filfcpo@group.calendar.google.com', timeMin=from_, timeMax=to_,
+                                          maxResults=25, singleEvents=True,
                                           orderBy='startTime').execute()
     events = events_result.get('items', [])
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    return events
 
 
 if __name__ == '__main__':
-    main()
+    events = get_today_events_from_google_calendar()
+
+    staff_txt = open("staffs.txt", 'w')
+    staffs = staff_txt.readlines()
+
+   if not events:
+        print('No upcoming events found.')
+    for event in events:
+        print(event)
+        # start = event['start'].get('dateTime', event['start'].get('date'))
+        # end = event['end'].get('dateTime', event['end'].get('date'))
+        # print(start, end, event['summary'])
