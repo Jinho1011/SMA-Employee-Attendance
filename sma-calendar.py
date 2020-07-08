2261c18cbaa1dff5e3b354c34ab92818b53471da
jinho1011 <gino9940@gmail.com>
2020-07-08 18:00:27 +0900

    Implement Except Break Hours from events in sma-calendar.py

--- sma-calendar.py
+++ sma-calendar.py
@@ -3,8 +3,8 @@ from pytz import timezone, utc
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-
-import datetime
+from dateutil.parser import parse
+from datetime import datetime, timedelta, date, timezone
 import pickle
 import os.path
 
@@ -31,11 +31,11 @@ def get_today_events_from_google_calendar():
 
     service = build('calendar', 'v3', credentials=creds)
 
-    today = datetime.date.today()
-    from_ = datetime.datetime(today.year, today.month, today.day, 0, 0, 0,
-                              tzinfo=datetime.timezone.utc).isoformat()
-    to_ = datetime.datetime(today.year, today.month, today.day, 11, 59, 59,
-                            tzinfo=datetime.timezone.utc).isoformat()
+    today = date.today()
+    from_ = datetime(today.year, today.month, today.day, 0, 0, 0,
+                     tzinfo=timezone.utc).isoformat()
+    to_ = datetime(today.year, today.month, today.day, 11, 59, 59,
+                   tzinfo=timezone.utc).isoformat()
 
     events_result = service.events().list(calendarId='pbinkbigkjdvo9tvb72filfcpo@group.calendar.google.com', timeMin=from_, timeMax=to_,
                                           maxResults=25, singleEvents=True,
@@ -51,14 +51,26 @@ if __name__ == '__main__':
     staff_txt = open("staffs.txt", 'r')
     staffs_origin = staff_txt.readlines()
     staffs = []
+    staffs_break = []
 
     for staff in staffs_origin:
         staff = staff.strip('\n')
         staffs.append("본 " + staff)
         staffs.append(staff)
+        staffs_break.append(staff + " 휴게시간")
 
     for event in events:
         if event['summary'] in staffs:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            end = event['end'].get('dateTime', event['end'].get('date'))
-            print(start, end, event['summary'])
+            start = parse(event['start']['dateTime'])
+            end = parse(event['end']['dateTime'])
+            # 휴게시간
+            if event['summary'].startswith("본"):
+                staff = event['summary'][2:]
+            else:
+                staff = event['summary']
+
+            print(staff)
+
+            # 점심시간
+
+            # work_hours = (end-start).seconds / 3600
